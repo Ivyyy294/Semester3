6350a9da339ee029ee0110f6a4a567c1fd062fdc
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-09-28 12:48:17 +0200

    ADM Initial commit

---
+++ NetworkClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetworkClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPAddress iPAddress = IPAddress.Parse ("127.0.0.1");
+			int port = 23000;
+
+			Console.WriteLine("Connecting to server...");
+			server.Connect (iPAddress, port);
+			Console.WriteLine("Succesful");
+
+			while (true)
+			{
+				string inputCommand = Console.ReadLine();
+
+				byte[] buffer = Encoding.ASCII.GetBytes (inputCommand);
+				server.Send (buffer);
+			}
+		}
+	}
+}
---
+++ NetworkServer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetworkServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			IPAddress iPAddress = IPAddress.Any;
+			IPEndPoint iPEndPoint = new IPEndPoint (iPAddress, 23000);
+			listenerSocket.Bind (iPEndPoint);
+
+			listenerSocket.Listen (5);
+
+			Console.WriteLine("Waiting for clients...");
+
+			Socket client = listenerSocket.Accept();
+
+			Console.WriteLine("Client connected. " + client.ToString()
+				+ ", IPEndpoint: " + client.RemoteEndPoint.ToString());
+
+			byte[] buffer = new byte [128];
+			int numberOfReceivedBytes = 0;
+
+			while (true)
+			{
+				numberOfReceivedBytes = client.Receive (buffer);
+
+				Console.WriteLine ("Number of received bytes: " + numberOfReceivedBytes);
+
+				string message = Encoding.ASCII.GetString (buffer, 0, numberOfReceivedBytes);
+
+				Console.WriteLine ("Data sent by clients: " + message);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
