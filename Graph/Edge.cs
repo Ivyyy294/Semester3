58101173bd7a2f4c8c252f88b653b32c667be4a9
Ivyyy294 <laraguelpen@gmail.com>
2023-10-18 16:59:51 +0200

    NEW Graph project

---
+++ Graph/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+	class Edge
+	{
+		Edge (Node _n1, Node _n2, float _weight)
+		{
+			n1 = _n1;
+			n2 = _n2;
+			weight = _weight;
+		}
+
+		private Node n1 = null;
+		private Node n2 = null;
+		private float weight;
+	}
+}
---
+++ Graph/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+	struct Position
+	{
+		float x;
+		float y;
+	}
+
+	class Node
+	{
+		public Node (string _name, Position _pos)
+		{
+			name = _name;
+			pos = _pos;
+		}
+
+		private string name;
+		private Position pos;
+
+		string GetName () { return name;}
+		Position GetPosition() { return pos;}
+	}
+}
