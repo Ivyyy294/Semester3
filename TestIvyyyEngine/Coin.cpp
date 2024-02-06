3153a5b570594adb3fd762830ff033e65a3e25b8
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2024-02-06 22:28:05 +0100

    :truck: Removed unused projects

--- TestIvyyyEngine/Coin.cpp
+++
@@ -1,8 +0,0 @@
-#include "Coin.h"
-#include "IvyyyEventSystem.h"
-
-void Coin::OnTriggerEnter ()
-{
-	gameObject->SetActive (false);
-	EventSystem::Me ()->Event ("coin").Invoke();
-}
--- TestIvyyyEngine/Player.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <IvyyyGameObject.h>
-class Player :
-	public Ivyyy::GameObject
-{
-public:
-	Player ();
-};
-
